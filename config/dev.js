2c98b1e3b63122b524e2c171be1c42c46a742caf
mesfandi <40899452+mesfandi@users.noreply.github.com>
2020-04-11 17:49:37 -0400

    final

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+// dev.js - dont commit this !!
+module.exports = {
+  mongoURI:
+    "mongodb+srv://javad:3uC9yNQeFoiwtBN0@cluster0-187zm.mongodb.net/hw-detabase?retryWrites=true&w=majority",
+  myURL: "localhost:3000/",
+};
--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,6 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookiekey: process.env.COOKIE_KEY
+  cookiekey: process.env.COOKIE_KEY,
+  myURL: "css645-hw3.herokuapp.com/",
 };
